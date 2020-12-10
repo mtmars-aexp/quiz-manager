927147d4de6e891498ac8648aa12ef5bd88184b7
mtmars-aexp <morgan.mars@aexp.com>
2020-12-10 16:12:30 +0000

    Test test tests. Writing some tests.

--- quiz-frontend/src/tests/QuizAnswerBox.test.js
+++ quiz-frontend/src/tests/QuizAnswerBox.test.js
@@ -1,7 +1,21 @@
 import React from 'react';
-import Enzyme, {shallow} from 'enzyme';
+import Enzyme, {shallow, mount} from 'enzyme';
 import QuizAnswerBox from '../components/QuizAnswerBox';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+class LocalStorageMock{
+    constructor(){
+        this.store = {};
+    }
+
+    getItem(key) {
+        return this.store[key] || null;
+    }
+    
+      setItem(key, value) {
+        this.store[key] = value.toString();
+    }
+}
 
 Enzyme.configure({adapter: new Adapter()});
 
@@ -29,5 +43,33 @@ describe('The question answer box', () => {
         expect(wrapper.text().includes("beep!")).toBe(true);
         expect(wrapper.text().includes("boop!")).toBe(true);
         expect(wrapper.text().includes("This is an answer to a question!")).toBe(true);
+    });
+    it('should call updateQuizAnswer from props when a radio button is clicked', () => {
+        const updateQuizAnswerMock = jest.fn();
+        const wrapper = mount(<QuizAnswerBox updateQuizAnswer={updateQuizAnswerMock}/>)
+        const answers = [{answer_id: 1, text: "beep!"}, {answer_id: 2, text: "boop!"}]
+        wrapper.setState({answers: answers})
+        wrapper.find("input").first().simulate('change');
+        expect(updateQuizAnswerMock).toHaveBeenCalledTimes(1);
+    });
+    it('should call toggleAnswerVisibility when the "Reveal Answer" button is clicked', () => {
+
+        //Setup localStorage mock.
+        global.localStorage = new LocalStorageMock;
+        localStorage.setItem("privilege", "2")
+
+        //Mount component and setup state.
+        const wrapper = mount(<QuizAnswerBox question_id={1}/>, { attachTo: document.body }) // Must attach to document body to be able to getElementById in function.
+        const answers = [{answer_id: 1, text: "beep!"}, {answer_id: 2, text: "boop!"}]
+        wrapper.setState({answers: answers})
+
+        //Setup spy.
+        const instance = wrapper.instance();
+        const toggleVisibilitySpy = jest.spyOn(instance, 'toggleAnswerVisibility')
+        wrapper.instance().forceUpdate();
+
+        //Assert.
+        wrapper.find('button').first().simulate('click')
+        expect(toggleVisibilitySpy).toHaveBeenCalled();
     });
 });
