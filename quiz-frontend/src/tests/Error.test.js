5d45f9185622742b2d695d1cc39dcc885c4d15ec
mtmars-aexp <morgan.mars@aexp.com>
2020-12-11 13:30:00 +0000

    Error test done

--- quiz-frontend/src/tests/Error.test.js
+++ quiz-frontend/src/tests/Error.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Enzyme, {shallow} from 'enzyme';
+import Error from '../components/error';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({adapter: new Adapter()});
 
-describe('CHANGEME', () => {
-    it('CHANGEME', () => {
-        expect(true).toBeTruthy();
+describe('The error page', () => {
+    it('should contain a link to the homepage.', () => {
+        //Setup wrapper
+        const wrapper = shallow(<Error/>)
+
+        expect(wrapper.find('[href="/"]')).toHaveLength(1);
     });
 });
